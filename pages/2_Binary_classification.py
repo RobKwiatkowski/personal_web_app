ade6337d3acab7f129d95107ec8c38c6424c596e
Robert <robert.kwiatkowski01@gmail.com>
2024-03-07 16:36:09 +0100

    first classifier

--- pages/2_Binary_classification.py
+++ pages/2_Binary_classification.py
@@ -9,6 +9,8 @@ from sklearn.metrics import precision_score, recall_score
 from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay, precision_recall_curve
 from sklearn.preprocessing import LabelEncoder
 
+pd.set_option("mode.copy_on_write", True)
+
 
 @st.cache_data(persist=True)
 def load_data_from_web():
@@ -24,32 +26,34 @@ def load_data_from_web():
 
 
 @st.cache_data(persist=True)
-def split(df):
-    y = df["poisonous"]
-    X = df.drop(columns=["poisonous"])
-    X_train, X_test, y_train, X_train = train_test_split(X, y, test_size=0.3, random_state=0)
-    return X_train, X_test, y_train, X_train
+def split_data(dataframe):
+    y = dataframe["poisonous"]
+    x = dataframe.drop(columns=["poisonous"])
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
+    return x_train, x_test, y_train, y_test
 
 
-def plot_metrics(metrics_list, y_test, predictions):
-    if "Confusion Matrix" in metrics_list:
-        st.subheader("Confusion Matrix")
-        ConfusionMatrixDisplay(y_test, predictions)
-        st.pyplot()
-    # if "Confusion Matrix" in metrics_list:
-    #     st.subheader("ROC Curve")
-    #     RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc, estimator_name='example estimator')
-    #     st.pyplot()
+# def plot_metrics(metrics_list, y_test, predictions):
+#     if "Confusion Matrix" in metrics_list:
+#         st.subheader("Confusion Matrix")
+#         ConfusionMatrixDisplay(y_test, predictions)
+#         st.pyplot()
+#     if "ROC curve" in metrics_list:
+#         pass
+#     if "Precision-Recall Curve" in metrics_list:
+#         pass
+#     #     st.subheader("ROC Curve")
+#     #     RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc, estimator_name='example estimator')
+#     #     st.pyplot()
 
 
 st.title("Binary Classification Web App")
 
-df = load_data_from_web()
-X_train, X_test, y_train, y_train = split(df)
+df_raw = load_data_from_web()
+
+X_train, X_test, y_train, y_test = split_data(df_raw)
 
 class_names = ["edible", "poisonous"]
-
-# plot_metrics()
 
 st.subheader("Choose classifier")
 classifier = st.selectbox("Classifier", options=["SVM", "LR", "RF"])
@@ -57,10 +61,19 @@ classifier = st.selectbox("Classifier", options=["SVM", "LR", "RF"])
 if classifier == "SVM":
     st.subheader("Select Hyperparameters")
     param_C = st.number_input("Regularization parameter C", 0.1, 10.0, step=0.1)
-    kernel = st.radio("Kernel type (Kernel Coefficient)", ["TBF", "Linear"], key="kernel")
+    kernel = st.radio("Kernel type (Kernel Coefficient)", ["rbf", "linear"], key="kernel")
     gamma = st.radio("Gamma", ["scale", "auto"])
+
+    metrics = st.multiselect("Coose evaluation metrics", ["Confusion Matrix", "ROC curve", "Precision-Recall Curve"])
+
+if st.button("Classify", key="classify"):
+    st.subheader("Support Vector Machine")
+    model = SVC(C=1, kernel="linear", gamma=0.9)
+    model.fit(X_train, y_train)
+    accuracy = model.score(X_test, y_test)
+    st.write(f"Accuracy:{accuracy}",)
 
 if st.checkbox("Show Raw Data", False):
     st.subheader("Mushroom Data Set")
-    st.write(df)
+    st.write(df_raw)
 
